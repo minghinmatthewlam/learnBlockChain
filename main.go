ebe5dfdd3cb8ea368453ed746826f8b61734b16f
Matthew Lam <matthewlam96@hotmail.com>
2022-07-15 10:17:33 -0700

    basic blockchain

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"main.go/blockchain"
+)
+
+func main() {
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("first block")
+	chain.AddBlock("second block")
+	chain.AddBlock("third block")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous hash %x\n", block.PrevHash)
+		fmt.Printf("data: %s\n", block.Data)
+		fmt.Printf("hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProofOfWork(block)
+		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
+
+}
